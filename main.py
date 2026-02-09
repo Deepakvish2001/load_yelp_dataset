753f4d744d9b4c372ced9ab44e2f1fda26cb6ff4
Deepak Vish <deepakvishwakarma1302@gmail.com>
2026-02-09 19:17:30 +0530

    add checkin & tip table

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from read_json import business_df
+from read_json import checkin_df
+from read_json import tip_df
 from db_connect import engine
 
 def load_dataframe(df, table_name):
@@ -11,4 +13,10 @@ def load_dataframe(df, table_name):
     print(f"{table_name} uploaded successfully 🚀")
 
 load_dataframe(business_df, 'business')
+print("done successfully")
+
+load_dataframe(checkin_df, 'checkin')
+print("done successfully")
+
+load_dataframe(tip_df, 'tip')
 print("done successfully")
--- read_json.py
+++ read_json.py
@@ -2,9 +2,24 @@ import pandas as pd
 import json
 
 file_path_business = r'C:\YELP Project\yelp_dataset\yelp_academic_dataset_business.json'
+file_path_checkin = r'C:\YELP Project\yelp_dataset\yelp_academic_dataset_checkin.json'
+file_path_tip = r'C:\YELP Project\yelp_dataset\yelp_academic_dataset_tip.json'
 
 with open(file_path_business,'r', encoding='utf-8') as f:
     business_data = [json.loads(line) for line in f]
 business_df = pd.DataFrame(business_data)
 print("json file loaded")
 business_df.drop(['attributes','hours'], axis=1, inplace=True)
+
+with open(file_path_checkin,'r', encoding='utf-8') as f:
+    checkin_data = [json.loads(line) for line in f]
+checkin_df = pd.DataFrame(checkin_data)
+print("json file loaded")
+
+with open(file_path_tip,'r', encoding='utf-8') as f:
+    tip_data = [json.loads(line) for line in f]
+tip_df = pd.DataFrame(tip_data)
+print("json file loaded")
+
+
+
