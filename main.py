d8d49c06367afb57083d37f72ef12954063dc175
Deepak Vish <deepakvishwakarma1302@gmail.com>
2026-02-09 21:32:24 +0530

    added review & user table

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from read_json import business_df
 from read_json import checkin_df
 from read_json import tip_df
+from read_json import review_df
+from read_json import user_df
 from db_connect import engine
 
 def load_dataframe(df, table_name):
@@ -19,4 +21,10 @@ load_dataframe(checkin_df, 'checkin')
 print("done successfully")
 
 load_dataframe(tip_df, 'tip')
+print("done successfully")
+
+load_dataframe(review_df, 'review')
+print("done successfully")
+
+load_dataframe(user_df, 'user')
 print("done successfully")
--- read_json.py
+++ read_json.py
@@ -4,6 +4,8 @@ import json
 file_path_business = r'C:\YELP Project\yelp_dataset\yelp_academic_dataset_business.json'
 file_path_checkin = r'C:\YELP Project\yelp_dataset\yelp_academic_dataset_checkin.json'
 file_path_tip = r'C:\YELP Project\yelp_dataset\yelp_academic_dataset_tip.json'
+file_path_review = r'C:\YELP Project\yelp_dataset\yelp_academic_dataset_review.json'
+file_path_user = r'C:\YELP Project\yelp_dataset\yelp_academic_dataset_user.json'
 
 with open(file_path_business,'r', encoding='utf-8') as f:
     business_data = [json.loads(line) for line in f]
@@ -21,5 +23,12 @@ with open(file_path_tip,'r', encoding='utf-8') as f:
 tip_df = pd.DataFrame(tip_data)
 print("json file loaded")
 
+with open(file_path_review,'r', encoding='utf-8') as f:
+    review_data = [json.loads(line) for line in f]
+review_df = pd.DataFrame(review_data)
+print("json file loaded")
 
-
+with open(file_path_user,'r', encoding='utf-8') as f:
+    user_data = [json.loads(line) for line in f]
+user_df = pd.DataFrame(user_data)
+print("json file loaded")
